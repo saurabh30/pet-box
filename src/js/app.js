bce6ab656ef465fb2bb057cb09426a08f0e7882b
saurabh30 <gupta.saurabh711@gmail.com>
2018-05-27 11:44:04 +0530

    handling adopt()

--- src/js/app.js
+++ src/js/app.js
@@ -70,11 +70,24 @@ App = {
     event.preventDefault();
 
     var petId = parseInt($(event.target).data('id'));
+    var adoptionInstance;
+    web3.eth.getAccounts(function(error,accounts){
+	if(error){
+	  console.log(error);		
+	}
 
-    /*
-     * Replace me...
-     */
-  }
+	var account = accounts[0];
+	
+	App.contracts.Adoption.deployed().then(function(instance){
+	  adoptionInstance = instance;
+	  return adoptionInstance.adopt(petId,{from:account});		
+	}).then(function(result){
+	  return App.markAdopted();
+	}).catch(function(err){
+	  console.log(err.message);	
+	});		
+	});
+ 
 
 };
 
